f3b0717b3c0849685df3b7168eaf3a63213dd16b
Naoyuki Totani <nyk.totani@gmail.com>
2017-02-02 00:10:05 +0900

    neomru

--- vim_install.sh
+++ vim_install.sh
@@ -3,6 +3,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/Shougo/neocomplcache.git
 git clone https://github.com/Shougo/unite.vim.git
+git clone https://github.com/Shougo/neomru.vim.git
 git clone https://github.com/kchmck/vim-coffee-script.git
 git clone https://github.com/kana/vim-smartchr.git
 git clone https://github.com/Shougo/vimfiler.git
