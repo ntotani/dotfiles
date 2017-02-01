e1315719793338e426aca86006bae18d2d10a2ce
Naoyuki Totani <nyk.totani@gmail.com>
2017-02-02 00:09:15 +0900

    fix

--- oh-my-zsh/custom/my.zsh
+++ oh-my-zsh/custom/my.zsh
@@ -1,6 +1,6 @@
 export LANG=ja_JP.UTF-8
 
-export PATH=~/bin:~/.rvm/bin:$PATH
+export PATH=~/bin:~/.rvm/bin:~/Library/Android/sdk/platform-tools:/usr/local/bin:$PATH
 export NODE_PATH=/usr/local/lib/node_modules:/usr/local/lib/node:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
 export EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
@@ -16,7 +16,7 @@ export ANT_ROOT=/usr/local/opt/ant/bin
 
 #source $(brew --prefix nvm)/nvm.sh
 #nvm use 0.11.14
-#if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 #export PYENV_ROOT=/usr/local/opt/pyenv
 source /Users/ntotani/.rvm/scripts/rvm
 
