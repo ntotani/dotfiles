7a8a50d7aed03af856ed523a65bc714fad3d50db
Naoyuki Totani <ntotani@NaoyukinoMacBook-Pro.local>
2020-02-21 18:11:20 +0900

    cocos

--- oh-my-zsh/custom/my.zsh
+++ oh-my-zsh/custom/my.zsh
@@ -19,6 +19,18 @@ if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 #export PYENV_ROOT=/usr/local/opt/pyenv
 source /Users/ntotani/.rvm/scripts/rvm
 
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+#export COCOS_CONSOLE_ROOT=/Applications/Cocos/Cocos2d-x/cocos2d-x-3.10/tools/cocos2d-console/bin
+#export PATH=$COCOS_CONSOLE_ROOT:$PATH
+
+# Add environment variable COCOS_X_ROOT for cocos2d-x
+#export COCOS_X_ROOT=/Applications/Cocos/Cocos2d-x
+#export PATH=$COCOS_X_ROOT:$PATH
+
+# Add environment variable COCOS_TEMPLATES_ROOT for cocos2d-x
+#export COCOS_TEMPLATES_ROOT=/Applications/Cocos/Cocos2d-x/cocos2d-x-3.10/templates
+#export PATH=$COCOS_TEMPLATES_ROOT:$PATH
+
 alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias gvim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -g --remote-tab-silent "$@"'
